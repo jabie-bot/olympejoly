1a01cbaac4051c34b59bd12a74cf3a8a161545fd
Olympe Joly <olympej@ik.me>
2024-11-19 21:17:32 +0100

    33

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,135 @@
-/*!
-* Start Bootstrap - Shop Item v5.0.6 (https://startbootstrap.com/template/shop-item)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-item/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+function filterBooks(category, clickedButton) {
+  // Récupère tous les boutons
+  var buttons = document.querySelectorAll('.btn');
+  
+  // Parcours tous les boutons
+  buttons.forEach(function(button) {
+    if (button === clickedButton) {
+      // Active le bouton actuel
+      button.classList.add('active-button');
+    } else {
+      // Désactive les autres boutons
+      button.classList.remove('active-button');
+    }
+  });
+
+  // Récupère tous les éléments avec la classe "portfolio-item"
+  var books = document.querySelectorAll('.portfolio-item');
+  
+  // Parcours tous les livres
+  books.forEach(function(book) {
+    // Vérifie si le livre a la classe correspondant à la catégorie
+    if (category === 'tous' || book.classList.contains(category)) {
+      book.style.display = 'block'; // Affiche le livre
+    } else {
+      book.style.display = 'none'; // Masque le livre
+    }
+  });
+}
+(function() {
+  "use strict"; // Start of use strict
+
+  var menuToggle = document.querySelector('.menu-toggle');
+  var sidebar = document.querySelector('#sidebar-wrapper');
+  
+  if (menuToggle) {
+    // Closes the sidebar menu
+    menuToggle.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      sidebar.classList.toggle('active');
+      menuToggle.classList.toggle('active');
+      
+      var icon = menuToggle.querySelector('.fa-bars, .fa-times');
+      
+      if (icon) {
+        if (icon.classList.contains('fa-times')) {
+          icon.classList.remove('fa-times');
+          icon.classList.add('fa-bars');
+        } else if (icon.classList.contains('fa-bars')) {
+          icon.classList.remove('fa-bars');
+          icon.classList.add('fa-times');
+        }
+      }
+
+    });
+  }
+
+  var navbarCollapse = document.querySelector('.navbar-collapse');
+
+  if (navbarCollapse) {
+    var navbarItems = navbarCollapse.querySelectorAll('a');
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    for (var item of navbarItems) {
+      item.addEventListener('click', function (event) {
+        sidebar.classList.remove('active');
+        menuToggle.classList.remove('active');
+        
+        var icon = menuToggle.querySelector('.fa-bars, .fa-times');
+      
+        if (icon) {
+          if (icon.classList.contains('fa-times')) {
+            icon.classList.remove('fa-times');
+            icon.classList.add('fa-bars');
+          } else if (icon.classList.contains('fa-bars')) {
+            icon.classList.remove('fa-bars');
+            icon.classList.add('fa-times');
+          }
+        }
+      });
+    }
+  }
+
+  // Scroll to top button appear
+  var scrollToTop = document.querySelector('.scroll-to-top');
+  
+  if (scrollToTop) {
+    
+    // Scroll to top button appear
+    window.addEventListener('scroll', function() {
+      var scrollDistance = window.pageYOffset;
+
+      if (scrollDistance > 100) {
+        scrollToTop.style.display = 'block';
+      } else {
+        scrollToTop.style.display = 'none';
+      }
+    });
+  }
+})(); // End of use strict
+
+// Disable Google Maps scrolling
+// See http://stackoverflow.com/a/25904582/1607849
+// Disable scroll zooming and bind back the click event
+var onMapMouseleaveHandler = function(e) {
+  this.addEventListener('click', onMapClickHandler);
+  this.removeEventListener('mouseleave', onMapMouseleaveHandler);
+
+  var iframe = this.querySelector('iframe'); 
+  
+  if (iframe) {
+    iframe.style.pointerEvents = 'none';
+  }
+}
+
+var onMapClickHandler = function(e) {
+  // Disable the click handler until the user leaves the map area
+  this.removeEventListener('click', onMapClickHandler);
+  // Handle the mouse leave event
+  this.addEventListener('mouseleave', onMapMouseleaveHandler);
+
+  // Enable scrolling zoom
+  var iframe = this.querySelector('iframe'); 
+  
+  if (iframe) {
+    iframe.style.pointerEvents = 'auto';
+  }
+}
+
+var maps = document.querySelectorAll('.map');
+
+for (var map of maps) {
+  // Enable map zooming with mouse scroll when the user clicks the map
+  map.addEventListener('click', onMapClickHandler);
+}
